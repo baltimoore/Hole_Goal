1cc2251f942bf64de533f7ab14971a40033850a2
Gatis Ieviņš <baltimoore@users.noreply.github.com>
2026-07-26 15:47:04 +0300

    now we also cover up the holes (i think?)

--- scenes/_game_scene/golfball_areas.gd
+++ scenes/_game_scene/golfball_areas.gd
@@ -1,29 +1,50 @@
 extends Area3D
 
-@export var collision_shape: CollisionShape3D
-@export var replacement_mesh: MeshInstance3D
-
 const GlassShatterAnimation = preload("res://resources/vfx/glass_explosion.tscn")
 const MetalShatterAnimation = preload("res://resources/vfx/metal_explosion.tscn")
+const BoardScene = preload("res://assets/board.tscn")
+
+@onready var boards_container = $"/root/GameUI/ViewportContainer/ConfigurableSubViewport/Level/City/Boards"
 
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
+	body_shape_entered.connect(_on_body_shape_entered)
+	
+func _on_body_shape_entered(
+	body_rid: RID,
+	body: Node3D,
+	body_shape_index: int,
+	local_shape_index: int
+) -> void:
+	if body is not Golfball:
+		return
 
-func _on_body_entered(body: Node3D) -> void:
-	if body == self: return
-	if body is Golfball:
-		SignalManager.golfball_goal.emit(get_meta("HoleValue"))
-		var explosion: Node3D
-		
-		match get_meta("HoleType"):
-			&"Window":
-				explosion = GlassShatterAnimation.instantiate()
-			&"Drainage":
-				explosion = MetalShatterAnimation.instantiate()
-			_:
-				print("Unmanaged hole type!")
-		
-		get_tree().current_scene.add_child(explosion)
-		explosion.global_position = body.global_position
-		explosion.explode()
-		body.queue_free()
+	var impact_shape_owner = shape_find_owner(local_shape_index)
+	var hit_shape = shape_owner_get_owner(impact_shape_owner) as CollisionShape3D
+	var impact_location = hit_shape.global_position
+
+	SignalManager.golfball_goal.emit(get_meta("HoleValue"))
+	_initiate_explosion(get_meta("HoleType"), impact_location)
+
+	body.queue_free()
+	_board_up_hole(hit_shape)
+
+func _initiate_explosion(hole_type:StringName, impact_location:Vector3)->void :#animate explosion
+	var explosion: Node3D
+	match get_meta("HoleType"):
+		&"Window":
+			explosion = GlassShatterAnimation.instantiate()
+		&"Drainage":
+			explosion = MetalShatterAnimation.instantiate()
+		_:
+			print("Unmanaged hole type!", get_meta("HoleType"))
+	get_tree().current_scene.add_child(explosion)
+	explosion.global_position = impact_location
+	explosion.explode()
+
+func _board_up_hole(shape_that_was_hit:CollisionShape3D) ->void:
+	var board = BoardScene.instantiate()
+	# this is incorrect, but fsfr, it's consistently too high
+	boards_container.add_child(board)
+	board.rotation = shape_that_was_hit.rotation
+	board.global_position = shape_that_was_hit.global_position
+	
